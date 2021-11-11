e76a94834ced4a61c60ab60934f850d44b5ea68d
Stefan <stefankaragiozov92@gmail.com>
2021-11-11 10:34:06 -0500

    added about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import PageContainer from "../components/pages-parts/PageContainer";
+import AboutImage from "../components/pages-parts/AboutImage";
 const About = () => {
-   
-        return (
-            <>
-            <Helmet>
-                <title>About page</title>
-                <meta name="description" content="travel friends about page"/>
-            </Helmet>
-            <h1>About page</h1>
-            </>
-        )
-    
-}
+  const [state] = useState({
+    heading: "about us",
+    pageHeading: "world's best travel agency company since 2005",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum inventore quae vitae. Ipsa natus iste vitae dolores pariatur earum quidem qui dignissimos! Enim minus quas, aperiam earum culpa ullam ad!  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum inventore quae vitae. Ipsa natus iste vitae dolores pariatur earum quidem qui dignissimos! Enim minus quas, aperiam earum culpa ullam ad!",
+  });
+  return (
+    <>
+      <Helmet>
+        <title>About page</title>
+        <meta name="description" content="travel friends about page" />
+      </Helmet>
+      <PageContainer data={state}> 
+        <AboutImage />
+      </PageContainer>
+    </>
+  );
+};
 export default About;
